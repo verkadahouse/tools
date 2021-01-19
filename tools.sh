5dc47e0b75a8c069f3aeb8d99fd9e42ea02bce12
verkadahouse <77704001+verkadahouse@users.noreply.github.com>
2021-01-19 15:37:32 -0800

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+brew install vlc 
+brew install --cask wireshark
+brew install --cask sublime-text
+brew install --cask rectangle
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask 1password
